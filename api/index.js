8e64ff18ffcf12f454f4baccbe9ccef643c13a3d
juanjure <64970269+juanjure@users.noreply.github.com>
2023-02-03 14:01:12 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
 const path = require('path')
 
+const port = process.env.PORT || 3000
 
 app.use(
     express.urlencoded({
-        extended: true
+        extended: false
     })
 )
 
